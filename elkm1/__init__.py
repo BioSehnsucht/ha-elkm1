419298547eb9a18691a512cc6f28013189cae2aa
Glenn Waters <gwwaters@gmail.com>
2018-08-28 22:41:34 -0400

    Bump elkm1-lib version

--- elkm1/__init__.py
+++ elkm1/__init__.py
@@ -21,7 +21,7 @@ from homeassistant.helpers.typing import ConfigType  # noqa
 
 DOMAIN = "elkm1"
 
-REQUIREMENTS = ['elkm1-lib==0.7.4']
+REQUIREMENTS = ['elkm1-lib==0.7.5']
 
 CONF_AREA = 'area'
 CONF_COUNTER = 'counter'
